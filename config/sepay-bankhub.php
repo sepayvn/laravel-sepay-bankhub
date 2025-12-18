ae8ff1a35f8591373b1c962fbcea45e5f8581665
Chung <nguyentranchung52th@gmail.com>
2025-12-18 12:40:13 +0900

    thêm config ipn_token

--- config/sepay-bankhub.php
+++ config/sepay-bankhub.php
@@ -4,4 +4,5 @@ return [
     'api_key' => env('SEPAY_BANKHUB_API_KEY'),
     'api_secret' => env('SEPAY_BANKHUB_API_SECRET'),
     'api_url' => env('SEPAY_BANKHUB_API_URL', 'https://partner-api.sepay.vn/merchant/v1'),
+    'ipn_token' => env('SEPAY_BANKHUB_IPN_TOKEN'),
 ];
